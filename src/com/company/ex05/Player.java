f793cba16381ce1798f883fa41b2cf74b1309ae3
lby132 <quddbscjswow@naver.com>
2024-04-10 19:45:47 +0900

    bubble

---
+++ src/com/company/ex05/Player.java
@@ -0,0 +1,124 @@
+package com.company.ex05;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.swing.*;
+
+@Getter
+@Setter
+public class Player extends JLabel implements Moveable {
+
+    // 위치 상태
+    private int x;
+    private int y;
+
+    // 움직임 상태
+    private boolean left;
+    private boolean right;
+    private boolean up;
+    private boolean down;
+
+    private final int SPEED = 4;
+    private final int JUMPSPEED = 2;
+
+    private ImageIcon playerR, playerL;
+
+    public Player() {
+        initObject();
+        initSetting();
+    }
+
+    private void initSetting() {
+        playerR = new ImageIcon("image/playerR.png");
+        playerL = new ImageIcon("image/playerL.png");
+    }
+
+    private void initObject() {
+        x = 55;
+        y = 535;
+
+        left = false;
+        right = false;
+        up = false;
+        down = false;
+
+        setIcon(playerR);
+        setSize(50, 50);
+        setLocation(x, y);
+    }
+
+    @Override
+    public void left() {
+        left = true;
+        new Thread(() -> {
+            while (left) {
+                setIcon(playerL);
+                x = x - SPEED;
+                setLocation(x, y);
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    public void right() {
+        right = true;
+        new Thread(() -> {
+            while (right) {
+                setIcon(playerR);
+                x = x + SPEED;
+                setLocation(x, y);
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    public void up() {
+        System.out.println("up");
+        up = true;
+        new Thread(() -> {
+            for (int i = 0; i < 130/JUMPSPEED; i++) {
+                y = y - JUMPSPEED;
+                setLocation(x, y);
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            up = false;
+            down();
+
+        }).start();
+    }
+
+    @Override
+    public void down() {
+        System.out.println("down");
+        down = true;
+        new Thread(() -> {
+            for (int i = 0; i < 130/JUMPSPEED; i++) {
+                y = y + JUMPSPEED;
+                setLocation(x, y);
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            down = false;
+        }).start();
+    }
+}
