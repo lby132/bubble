09e9d4a2bbfbf188ef3e1e48ffbd5aa4ec61f8fd
lby132 <quddbscjswow@naver.com>
2024-05-05 19:18:01 +0900

    bubble

--- src/com/company/ex06/BackgroundPlayerService.java
+++ src/com/company/ex06/BackgroundPlayerService.java
@@ -29,6 +29,14 @@ public class BackgroundPlayerService implements Runnable {
         while (true) {
             Color leftColor = new Color(image.getRGB(player.getX() - 10, player.getY() + 25));
             Color rightColor = new Color(image.getRGB(player.getX() + 50 + 15, player.getY() + 25));
+//            System.out.println("leftColor = " + leftColor);
+//            System.out.println("rightColor = " + rightColor);
+
+            if (leftColor.getRed() == 255 && leftColor.getGreen() == 0 && leftColor.getBlue() == 0) {
+                System.out.println("왼쪽 벽에 출동함");
+            } else if (rightColor.getRed() == 255 && rightColor.getGreen() == 0 && rightColor.getBlue() == 0) {
+                System.out.println("오른쪽 벽에 출동함");
+            }
 
             try {
                 Thread.sleep(10);
